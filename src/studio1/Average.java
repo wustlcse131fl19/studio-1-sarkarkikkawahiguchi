d7ac81ef4abeda7aeb0964ab366ba5e969cbcce4
stephaniesarkar <srsarkar@wustl.edu>
2019-08-29 13:52:14 -0500

    finished average

--- src/studio1/Average.java
+++ src/studio1/Average.java
@@ -5,7 +5,6 @@ import support.cse131.ArgsProcessor;
 public class Average {
     public static void main (String[] args){
         ArgsProcessor ap = new ArgsProcessor(args);
-
         int n1 = ap.nextInt("The first of two integers to be averaged?");
         int n2 = ap.nextInt("The second of two integers to be averaged?");
         int add = n1+n2;
